a12117e2ce42c8ce8cf4162a88ce9790d9e10cb9
aimthiazz <aimthiazz@gmail.com>
2021-01-09 16:54:26 -0600

    Implemeted changes
    Also Added main.py to test the features

---
+++ python/model_obj/Dimensions/dim_degree.py
@@ -0,0 +1,24 @@
+from dim_angular import DimAngular
+
+
+class DimDegree(DimAngular):
+    def __new__(cls, value):
+        if type(value).__bases__[0] is DimAngular:
+            value = value.to_degrees()
+        return DimAngular.__new__(cls, value)
+
+    def to_degrees(self):
+        return self
+
+    def to_radians(self):
+        return self*3.14/180
+
+
+    # def _to_dimensionless(self):
+    #     return float(self)
+    #
+    # def _from_dimensionless(num1):
+    #     x = num1
+    #     return DimDegree(x)
+
+        
