ed45e258afab0691253ea451350d47408ee5fca3
Martin Johnson <mjohnson56@wisc.edu>
2021-02-18 11:49:59 -0600

    add BSPM_machine Class

--- Machine.py
+++ Machine.py
@@ -12,7 +12,37 @@ class Machine(ABC):
     def __init__(self):
         pass
     
-        
+    @abstractmethod
+    def required_nameplate():
+        pass
+    
+    @abstractmethod
+    def check_required_values():
+        pass
+    
+    @abstractmethod
+    def get_missing_required_values():
+        pass
+    
+class MachineComponent(ABC):
+    """Base Class for Machine Components"""
+    
+    @abstractmethod
+    def required_geometry():
+        pass
+    
+    @abstractmethod
+    def required_materials():
+        pass
+
+class Winding(MachineComponent):
+    """Base Class for Machine Components"""
+    
+    @abstractmethod
+    def required_winding():
+        pass
+
+    
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
