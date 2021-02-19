42971d05bb95a8291620fe966526132688ab8f96
Bharat Ramadas <bramadas@wisc.edu>
2021-02-18 23:37:02 -0600

    Add support for BSPM Architect

---
+++ specifications/materials/miscellaneous_materials.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 19:19:23 2021
+
+@author: Bharat
+"""
+
+CarbonFiber = {
+    'sleeve_material'            : 'CarbonFiber',
+    'sleeve_material_density'    : 1800, # kg/m3
+    'sleeve_youngs_th_direction' : 125E9,  #Pa
+    'sleeve_youngs_p_direction'  : 8.8E9,  #Pa
+    'sleeve_poission_ratio_p'    :.015,
+    'sleeve_poission_ratio_tp'   :.28,
+    'sleeve_safety_factor'       : 1.5, #
+    'sleeve_max_tan_stress'      : 1950E6, # Pa
+    'sleeve_max_rad_stress'      : -100E6, # Pa
+    'sleeve_therm_conductivity'  : 0.71, # W/m-k
+    }
+
+Steel = {
+    'shaft_material'             : 'Steel',
+    'shaft_material_density'     : 7870, # kg/m3
+    'shaft_youngs_modulus'       : 206E9, #Pa
+    'shaft_poission_ratio'       : .3, #[]
+    'shaft_therm_conductivity'   : 51.9, # W/m-k  
+    }
+
+Copper = {
+    'copper_material_cost'       : 73228, # $/m3
+    'copper_elec_conductivity'   : 5.7773*1e7, # S/m
+    }
+
+Air = {
+    'air_therm_conductivity'     :.02624, #W/m-K
+    'air_viscosity'              :1.562E-5, #m^2/s
+    'air_cp'                     :1, #kJ/kg
+    'air_temp'                   :25, #[C]
+    }
+
+Hub = {
+       'rotor_hub_therm_conductivity':205.0, #W/m-K       
+      }
