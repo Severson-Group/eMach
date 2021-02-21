af07c9d4dbb63695231141271005184888e306f2
Bharat Ramadas <bramadas@wisc.edu>
2021-02-21 16:14:00 -0600

    Update function signature to be eMach compliant

--- emach/tools/tool_abc/toolabc.py
+++ emach/tools/tool_abc/toolabc.py
@@ -8,13 +8,11 @@ Created on Fri Oct 16 16:48:15 2020
 from abc import ABC, abstractmethod
 from typing import List
 
-from ...model_obj import Location2D
-
 
 # define abstract base class for ToolBase
 class ToolBase(ABC):
     @abstractmethod
-    def open(self, filename: str, visible: bool) -> None: pass
+    def open(self, filename: str, visible: bool) -> any: pass
     
     @abstractmethod
     def close(self): pass
@@ -22,26 +20,28 @@ class ToolBase(ABC):
 # define abstract base class for DrawerBase
 class DrawerBase(ABC):
     @abstractmethod
-    def draw_line(self, startxy: Location2D, endxy: Location2D) -> int: pass
+    def draw_line(self, startxy: 'Location2D', endxy: 'Location2D') \
+    -> 'TokenDraw': pass
     
     @abstractmethod
-    def draw_arc(self, centerxy: Location2D, startxy: Location2D, 
-                 endxy: Location2D) -> int: pass
+    def draw_arc(self, centerxy: 'Location2D', startxy: 'Location2D', \
+                 endxy: 'Location2D') -> 'TokenDraw': pass
 
     @abstractmethod
     def select(self): pass
 
 class MakerBase(ABC):
     @abstractmethod
-    def prepare_section(self, inner_coord: Location2D) -> None: pass
+    def prepare_section(self, inner_coord: 'Location2D') -> any: pass
 
 # define abstract base class for MakerExtrudeBase
 class MakerExtrudeBase(MakerBase):
     @abstractmethod
-    def extrude(self, name: List[str], material: str, depth: float, token) -> int: pass
+    def extrude(self, name: List[str], material: str, depth: float) -> any:
+        pass
 
 # define abstract base class for MakerRevolveBase 
 class MakerRevolveBase(MakerBase):
     @abstractmethod
-    def revolve(self, name: List[str], material: str, center: Location2D, 
-                axis: Location2D, angle: float, token): pass
+    def revolve(self, name: List[str], material: str, center: 'Location2D', \
+                axis: 'Location2D', angle: float) -> any: pass
