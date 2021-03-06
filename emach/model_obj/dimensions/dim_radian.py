632669056b037803d97c04bb9c130e6ffdaeb501
Bharat Ramadas <bramadas@wisc.edu>
2021-03-06 13:12:41 -0600

    Update dimensions

--- emach/model_obj/dimensions/dim_radian.py
+++ emach/model_obj/dimensions/dim_radian.py
@@ -4,22 +4,8 @@ __all__ = ['DimRadian']
 
 class DimRadian(DimAngular):
     def __new__(cls, value):
-        if type(value).__bases__[0] is DimAngular:
-            value = value.to_radians()
         return DimAngular.__new__(cls, value)
 
-    def to_degrees(self):
-        return self*180/3.14
-
-    def to_radians(self):
-        return self
-    
-    
-
-
-    # def _to_dimensionless(self):
-    #     return float(self.data * 180/3.14)
-    #
-    # def _from_dimensionless(num1):
-    #     x = num1*3.14/ 180
-    #     return DimRadian(x)
+    @property
+    def conversion_factor(self):
+        return 1
