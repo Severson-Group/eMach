19e6eaadbbe97562ccc8e4834764fa74b2b20264
aimthiazz <aimthiazz@gmail.com>
2021-03-21 15:03:50 -0500

    Create test_dim_linear_multiply.py

---
+++ emach/tests/dimensions/test_dim_linear_multiply.py
@@ -0,0 +1,41 @@
+import unittest
+
+from eMach.python.emach.model_obj import DimInch, DimMillimeter
+
+oneInch = DimInch(1)
+twoInches = DimInch(2)
+
+oneMillimeter = DimMillimeter(1)
+twoMillimeters = DimMillimeter(2)
+
+
+class TestMultiply(unittest.TestCase):
+    def test_multiply_scalar_lhs(self):
+        self.assertAlmostEqual((2 * oneInch), DimInch(2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(2 * oneInch), type(DimInch(2)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((2 * oneMillimeter), DimMillimeter(2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(2 * oneMillimeter), type(DimMillimeter(2)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((0 * oneMillimeter), DimMillimeter(0), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(0 * oneMillimeter), type(DimMillimeter(0)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((-2 * oneMillimeter), DimMillimeter(-2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(-2 * oneMillimeter), type(DimMillimeter(-2)), 'Inch Multiplication Fail')
+
+    def test_multiply_scalar_rhs(self):
+        self.assertAlmostEqual((oneInch * 2), DimInch(2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(oneInch * 2), type(DimInch(2)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((oneMillimeter * 2), DimMillimeter(2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(oneMillimeter * 2), type(DimMillimeter(2)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((oneMillimeter * 0), DimMillimeter(0), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(oneMillimeter * 0), type(DimMillimeter(0)), 'Inch Multiplication Fail')
+
+        self.assertAlmostEqual((oneMillimeter * -2), DimMillimeter(-2), 5, 'Inch Multiplication Fail')
+        self.assertEqual(type(oneMillimeter * -2), type(DimMillimeter(-2)), 'Inch Multiplication Fail')
+
+
+if __name__ == '__main__':
+    unittest.main()
