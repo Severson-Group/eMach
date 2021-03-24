e17988252842151bddbe9f22b1422d274a110e65
aimthiazz <aimthiazz@gmail.com>
2021-03-23 20:13:41 -0500

    Create test_dim_linear_power.py

---
+++ emach/tests/dimensions/test_dim_linear_power.py
@@ -0,0 +1,22 @@
+import unittest
+
+import model_obj
+from model_obj.dimensions import DimInch, DimMillimeter
+
+oneInch = DimInch(1)
+
+
+class TestPower(unittest.TestCase):
+    def test_fail_conditions_divide(self):
+        with self.assertRaises(Exception):
+            oneInch ** 2
+
+        with self.assertRaises(Exception):
+            2 ** oneInch
+
+        with self.assertRaises(Exception):
+            oneInch ** oneInch
+
+
+if __name__ == '__main__':
+    unittest.main()
