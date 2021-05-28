49e111f74e0af3cf341540dcde1ce5633842b721
Imthiaz Ahmed <aimthiazz@gmail.com>
2021-05-28 14:02:19 -0500

    Update __init__.py

--- emach/model_obj/cross_sects/__init__.py
+++ emach/model_obj/cross_sects/__init__.py
@@ -1,15 +1,31 @@
 
 from .hollow_cylinder import *
-
 from .notched_rectangle import *
-
+from .trapezoid import *
+from .solid_rectangle import *
+from .parallelogram import *
 from .outer_rotor_stator import *
+from .outer_rotor import *
+from .linear_motor_stator import *
+from .inner_rotor_stator import *
+from .inner_notched_rectangle import *
+from .hollow_rectangle import *
+from .breadloaf import *
+from .arc import*
 
 
 __all__ = []
 __all__ += hollow_cylinder.__all__
 
-
 __all__ += notched_rectangle.__all__
-
+__all__ += trapezoid.__all__
+__all__ += solid_rectangle.__all__
+__all__ += parallelogram.__all__
 __all__ += outer_rotor_stator.__all__
+__all__ += outer_rotor.__all__
+__all__ += linear_motor_stator.__all__
+__all__ += inner_rotor_stator.__all__
+__all__ += inner_notched_rectangle.__all__
+__all__ += hollow_rectangle.__all__
+__all__ += breadloaf.__all__
+__all__ += arc.__all__
