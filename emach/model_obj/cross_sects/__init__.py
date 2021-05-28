000d1944c2523ed7b7bc1dfb30fc92cc870513fc
Bharat Ramadas <bramadas@wisc.edu>
2021-05-28 15:57:28 -0500

    Add cross-sections

--- emach/model_obj/cross_sects/__init__.py
+++ emach/model_obj/cross_sects/__init__.py
@@ -1,8 +1,16 @@
-
 from .hollow_cylinder import *
 from .notched_rectangle import *
+from .arc import *
+from .breadloaf import *
+from .hollow_rectangle import *
+from .inner_notched_rotor import *
+from .inner_rotor_stator import *
 
 __all__ = []
 __all__ += hollow_cylinder.__all__
-
+__all__ += arc.__all__
+__all__ += breadloaf.__all__
+__all__ += hollow_rectangle.__all__
+__all__ += inner_notched_rotor.__all__
+__all__ += inner_rotor_stator.__all__
 __all__ += notched_rectangle.__all__
