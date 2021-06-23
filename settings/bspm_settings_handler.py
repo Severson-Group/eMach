e38e581c1894bb1f9a089996a1643f322b83703f
Bharat Ramadas <bramadas@wisc.edu>
2021-06-22 21:39:34 -0500

    Add em analyzer with example to LegacyCode

---
+++ settings/bspm_settings_handler.py
@@ -0,0 +1,14 @@
+from .bspm_settings import BSPM_EMAnalyzer_Settings
+
+class BSPM_Settings_Handler():
+    ''' This is a wrapper class designed to contain all relevant information
+    on the operting point for obtaining eletrical performance evaluation of 
+    bearingless permanent magnet eletric machines
+    '''
+    def __init__(self):
+        pass
+    
+    def get_settings(self,x):
+        em_op = BSPM_EMAnalyzer_Settings(Id=x[11], Iq=x[12], Ix=x[13], Iy=x[13],\
+                                         speed=x[14], magnet_temp=x[15])
+        return em_op
