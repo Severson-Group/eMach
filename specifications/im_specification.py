9a69dd650baf8298c8f27d6d9461166ac50a2cd3
Imthiaz Ahmed <aimthiazz@gmail.com>
2021-08-09 22:13:14 -0500

    Update im_specification.py

--- specifications/im_specification.py
+++ specifications/im_specification.py
@@ -6,13 +6,14 @@ class IMMachineSpec(Specification):
     This class is a wrapper for all information required by IM_Machine Architects
     '''
     
-    def __init__(self,design_spec, rotor_core, stator_core , magnet , \
-                 conductor, shaft, air = None, sleeve = None, hub = None):
+    
+    def __init__(self,design_spec, rotor_core, stator_core , rotor_bar , \
+                 conductor, shaft, air = None, hub = None):
         self.design_spec = design_spec
         self.rotor_material = rotor_core
         self.stator_material = stator_core
-        self.magnet_material = magnet
-        self.sleeve_material = sleeve
+        self.rotor_bar_material = rotor_bar
+
         self.coil_material = conductor
         self.shaft_material = shaft
         self.rotor_hub = hub
