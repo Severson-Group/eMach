168524615c3fbcba4680300c43d2ce5dd3f85481
Imthiaz Ahmed <aimthiazz@gmail.com>
2021-08-17 11:48:30 -0500

    Add im_spec and update git ignore

---
+++ specifications/machine_specs/im1_machine_specs.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+
+#IM Q24p1y9 Qr30-FSW Round Bar EquivDoubleLayer TEST
+DesignSpec = {
+    "machine_type": "IM",
+    "PS_or_SC": True,
+    "DPNV_or_SEPA": True,
+    "PoleSpecificNeutral": True,
+    "m": 3,
+    "p": 3,
+    "ps": 4,
+    "coil_pitch_y": 3,
+    "coil_pitch_y_Qr": 3,
+    "mec_power": 50e3,
+    "ExcitationFreqSimulated": 1500,
+    "VoltageRating": 480,
+    "TangentialStress": 12000,
+    "Qs": 18,
+    "Qr": 12,
+    "Js": 4e6,
+    "Jr": 6.5e6,
+    "Steel": "M19Gauge29",
+    "lamination_stacking_factor_kFe": 0.95,
+    "Coil": "Cu",
+    "space_factor_kCu": 0.5,
+    "Conductor": "Cu",
+    "space_factor_kAl": 1.0,
+    "Temperature": 75,
+    "stator_tooth_flux_density_B_ds": 1.4,
+    "rotor_tooth_flux_density_B_dr": 1.5,
+    "stator_yoke_flux_density_Bys": 1.2,
+    "rotor_yoke_flux_density_Byr": 1.4,
+    "guess_air_gap_flux_density": 0.8,
+    "guess_efficiency": 0.95,
+    "guess_power_factor": 0.6,
+    "use_drop_shape_rotor_bar": False,
+    "tip_speed": 150,
+    "safety_factor_to_yield": "None",
+    "safety_factor_to_critical_speed": 1.5,
+    "bool_skew_stator": False,
+    "bool_skew_rotor": False
+    }
+
