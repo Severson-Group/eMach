e08b77b188267ccf1d16d3bbaed697bbbbf6388b
Imthiaz Ahmed <aimthiazz@gmail.com>
2021-08-18 11:00:19 -0500

    Update main code and specs

--- LegacyCode/main_im.py
+++ LegacyCode/main_im.py
@@ -28,75 +28,77 @@ from mach_eval import AnalysisStep, State, MachineDesigner, MachineEvaluator
 ############################ Define Design ###################################
 ##############################################################################
 
+
 # create specification object for the BSPM machine
-machine_spec = IMMachineSpec(design_spec=DesignSpec, rotor_core=Arnon5,
-                               stator_core=Arnon5, magnet=N40H, conductor=Copper,
-                               shaft=Steel, air=Air, sleeve=CarbonFiber, hub=Hub)
+machine_spec = IMMachineSpec(design_spec=DesignSpec, rotor_core=DesignSpec["Steel"],
+                               stator_core=DesignSpec["Steel"], rotor_bar=DesignSpec["bar"], conductor=DesignSpec["coil"],
+                               shaft=Air, air=Air, hub=Hub)
 
+print("Steel Material", type(DesignSpec["Steel"]))
 # initialize BSPMArchitect with machine specification
-arch = BSPMArchitectType1(machine_spec)
-set_handler = BSPM_Settings_Handler()
-
-bspm_designer = MachineDesigner(arch, set_handler)
-# create machine variant using architect
-free_var = (0.00390399, 0.00964596, 35.9925, 0.00358376, 0.00722451, 0.0128492,
-            0.0143288, 180.0, 0.00514122, 0.00308507, 0.00363824, 0.0, 0.95, 0,
-            0.05, 200000, 80)
-# set operating point for BSPM machine
-
-design_variant = bspm_designer.create_design(free_var)
-
-##############################################################################
-############################ Define struct AnalysisStep ######################
-##############################################################################
-
-stress_limits={'rad_sleeve': -100E6,
-               'tan_sleeve': 1300E6,
-               'rad_magnets': 0,
-               'tan_magnets': 80E6}
-
-# spd = sta.SleeveProblemDef(design_variant)
-# problem = spd.get_problem()
-ana = sta.SleeveAnalyzer(stress_limits)
-# sleeve_dim = ana.analyze(problem)
-# print(sleeve_dim)
-
-
-class StructPostAnalyzer:
-    """Converts a State into a problem"""
-    def __init__(self):
-        pass
-
-    def get_next_state(results, in_state):
-        state_out = in_state
-        return state_out
-
-
-struct_step = AnalysisStep(sta.SleeveProblemDef, ana, StructPostAnalyzer)
-
-##############################################################################
-############################ Define em AnalysisStep ##########################
-##############################################################################
-
-
-class BSPM_EM_ProblemDefinition():
-    """Converts a State into a problem"""
-
-    def __init__(self):
-        pass
-
-    def get_problem(state):
-        problem = BSPM_EM_Problem(state.design.machine, state.design.settings)
-        return problem
-
-
-# initialize em analyzer class with FEA configuration
-em_analysis = BSPM_EM_Analysis(JMAG_FEA_Configuration)
-
-# define em step
-em_step = AnalysisStep(BSPM_EM_ProblemDefinition, em_analysis, BSPM_EM_PostAnalyzer)
-
-# evaluate machine design
-evaluator = MachineEvaluator([struct_step, em_step, LengthScaleStep])
-results = evaluator.evaluate(design_variant)
-
+# arch = BSPMArchitectType1(machine_spec)
+# set_handler = BSPM_Settings_Handler()
+#
+# bspm_designer = MachineDesigner(arch, set_handler)
+# # create machine variant using architect
+# free_var = (0.00390399, 0.00964596, 35.9925, 0.00358376, 0.00722451, 0.0128492,
+#             0.0143288, 180.0, 0.00514122, 0.00308507, 0.00363824, 0.0, 0.95, 0,
+#             0.05, 200000, 80)
+# # set operating point for BSPM machine
+#
+# design_variant = bspm_designer.create_design(free_var)
+#
+# ##############################################################################
+# ############################ Define struct AnalysisStep ######################
+# ##############################################################################
+#
+# stress_limits={'rad_sleeve': -100E6,
+#                'tan_sleeve': 1300E6,
+#                'rad_magnets': 0,
+#                'tan_magnets': 80E6}
+#
+# # spd = sta.SleeveProblemDef(design_variant)
+# # problem = spd.get_problem()
+# ana = sta.SleeveAnalyzer(stress_limits)
+# # sleeve_dim = ana.analyze(problem)
+# # print(sleeve_dim)
+#
+#
+# class StructPostAnalyzer:
+#     """Converts a State into a problem"""
+#     def __init__(self):
+#         pass
+#
+#     def get_next_state(results, in_state):
+#         state_out = in_state
+#         return state_out
+#
+#
+# struct_step = AnalysisStep(sta.SleeveProblemDef, ana, StructPostAnalyzer)
+#
+# ##############################################################################
+# ############################ Define em AnalysisStep ##########################
+# ##############################################################################
+#
+#
+# class BSPM_EM_ProblemDefinition():
+#     """Converts a State into a problem"""
+#
+#     def __init__(self):
+#         pass
+#
+#     def get_problem(state):
+#         problem = BSPM_EM_Problem(state.design.machine, state.design.settings)
+#         return problem
+#
+#
+# # initialize em analyzer class with FEA configuration
+# em_analysis = BSPM_EM_Analysis(JMAG_FEA_Configuration)
+#
+# # define em step
+# em_step = AnalysisStep(BSPM_EM_ProblemDefinition, em_analysis, BSPM_EM_PostAnalyzer)
+#
+# # evaluate machine design
+# evaluator = MachineEvaluator([struct_step, em_step, LengthScaleStep])
+# results = evaluator.evaluate(design_variant)
+#
