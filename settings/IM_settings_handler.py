75e2d85fde11edac875399bd934fd4c0c40ed298
Imthiaz Ahmed <aimthiazz@gmail.com>
2021-08-24 12:20:00 -0500

    Debug code

--- settings/IM_settings_handler.py
+++ settings/IM_settings_handler.py
@@ -1,6 +1,6 @@
-from .IM_settings import IM_EMAnalyzer_Settings
+from .im_settings import IM_EMAnalyzer_Settings
 
-class BSPM_Settings_Handler():
+class IM_Settings_Handler():
     ''' This is a wrapper class designed to contain all relevant information
     on the operting point for obtaining eletrical performance evaluation of 
     bearingless permanent magnet eletric machines
@@ -10,6 +10,7 @@ class BSPM_Settings_Handler():
         pass
     
     def get_settings(self,x):
-        em_op = BSPM_EMAnalyzer_Settings(Id=x[11], Iq=x[12], Ix=x[13], Iy=x[14],
-                                         speed=x[15], magnet_temp=x[16])
-        return em_op
+        # em_op = BSPM_EMAnalyzer_Settings(Id=x[11], Iq=x[12], Ix=x[13], Iy=x[14],
+        #                                  speed=x[15], magnet_temp=x[16])
+        # return em_op
+        return None
