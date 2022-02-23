d77a34f4b2a81964ce51e0358de8e45b79ad65c4
bharat-ramadas <bramadas@ad.engr.wisc.edu>
2022-02-23 12:14:56 -0600

    Delete file copies

--- examples/LegacyCode/post_analyzers/post_analyzer_base.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol
-from abc import abstractmethod
-
-class PostAnalyzer(Protocol):
-    @abstractmethod
-    def get_next_state(self,results:'Results',stateIn:'State')->'State':
-        pass
--- examples/LegacyCode/specifications/materials/rotor_bar.py
+++
@@ -1,5 +0,0 @@
-
-
-Aluminium = {
-    'BarConductivity' : 1e6 # putting random values for now
-}
