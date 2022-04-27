f5607b6820067c9ec94e85ea011c85d9d70eda49
Bharat Ramadas <70265814+Bharat-Ramadas@users.noreply.github.com>
2022-04-27 01:03:09 -0500

    Update mach_eval.py

--- mach_eval/mach_eval.py
+++ mach_eval/mach_eval.py
@@ -8,7 +8,7 @@ from typing import Protocol, runtime_checkable, Any, List
 from abc import abstractmethod, ABC
 import sys
 sys.path.append("..")
-from .. import mach_opt as mo
+import mach_opt as mo
 from copy import deepcopy
 
 __all__ = [
