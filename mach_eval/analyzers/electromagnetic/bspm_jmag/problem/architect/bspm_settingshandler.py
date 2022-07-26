582bb3ccbd7cff6fc9bdc3a5f83274b9054d88fc
Bharat Ramadas <bramadas@wisc.edu>
2022-07-26 10:49:42 -0500

    Update mach eval folder structure

---
+++ mach_eval/analyzers/electromagnetic/bspm_jmag/problem/architect/bspm_settingshandler.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 11:58:20 2021
+
+@author: Martin Johnson
+"""
+
+import sys
+sys.path.append("...")
+import macheval as me
+
+class BSPMSettingsHandler(me.SettingsHandler):
+    def getSettings(x):
+        return NotImplementedError #TODO Implement settings functionality
+    
