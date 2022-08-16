0f6e078ae32469804a1030a70e8392a5453403a1
Bharat Ramadas <bramadas@wisc.edu>
2022-08-16 00:23:56 -0500

    Add BField protocol

--- mach_eval/analyzers/electromagnetic/bfield_spm_inner_rotor.py
+++ mach_eval/analyzers/electromagnetic/bfield_spm_inner_rotor.py
@@ -5,12 +5,17 @@
 ###############################################################################
 
 import numpy as np
+import os
+import sys
 
+sys.path.append(os.path.dirname(__file__))
+from bfield_protocol import BField
 
-class SPM_InnerRotorPMFieldProblem:
+class BFieldSPM_InnerRotorProblem:
     """Problem class for stator radial B field analyzer
     Attributes:
         alpha_p: angular length of magnet in pu
+        theta: orientation of rotor d-axis
         p: Number of pole pairs
         muR: Relative permeability
         r_fe: Outer radius of rotor iron
@@ -19,8 +24,9 @@ class SPM_InnerRotorPMFieldProblem:
         mag_dir: Direction of magnetization, 'parallel' or 'radial'
     """
 
-    def __init__(self, alpha_p, p, muR, Br, r_fe, dm, delta, mag_dir):
+    def __init__(self, alpha_p, theta, p, muR, Br, r_fe, dm, delta, mag_dir):
         self.alpha_p = alpha_p
+        self.theta = theta
         self.p = p
         self.muR = muR
         self.Br=Br
@@ -30,7 +36,7 @@ class SPM_InnerRotorPMFieldProblem:
         self.mag_dir = mag_dir
 
 
-class SPM_InnerRotorPMFieldAnalyzer:
+class BFieldSPM_InnerRotorAnalyzer:
     """Analyzer class to evaluate SPM arc magnet radial B field"""
 
     def analyze(self, problem=None):
@@ -42,8 +48,9 @@ class SPM_InnerRotorPMFieldAnalyzer:
             b_field: Radial magnetic field at radius r from stator windings [T]
         """
 
-        b_field = SPM_InnerRotorPMField(
+        b_field = BFieldSPM_InnerRotor(
             alpha_p=problem.alpha_p,
+            theta=problem.theta,
             p=problem.p,
             muR=problem.muR,
             Br=problem.Br,
@@ -55,7 +62,7 @@ class SPM_InnerRotorPMFieldAnalyzer:
         return b_field
 
 
-class SPM_InnerRotorPMField:
+class BFieldSPM_InnerRotor(BField):
     """Class representing radial B field across motor airgap of SPM from arc magnets
 
     Attributes:
@@ -68,8 +75,9 @@ class SPM_InnerRotorPMField:
         mag_dir: Direction of magnetization, 'parallel' or 'radial'
     """
 
-    def __init__(self, alpha_p, p, muR, Br, r_fe, dm, delta, mag_dir):
+    def __init__(self, alpha_p, theta, p, muR, Br, r_fe, dm, delta, mag_dir):
         self.alpha_p = alpha_p
+        self.theta = theta
         self.p = p
         self.muR = muR
         self.Br=Br
@@ -80,24 +88,23 @@ class SPM_InnerRotorPMField:
         self.Rmo = r_fe+dm
         self.Rsi = r_fe+dm+delta
 
-    def radial(self, alpha: np.array, r, theta, harmonics):
+    def radial(self, alpha, r=None, harmonics=None):
         """Determines radial B field at angle(s) alpha and radius r
 
         Args:
             alpha: A numpy array holding angles at which B field is calculated.
-            r: Radius at which B field is calculated. Should be of type int or float
-            theta: angular orientation of PM rotor d-axis
-            harmonics: A numpy array holding holding harmonics of interest
+            r: Radius at which B field is calculated.
+            theta: Angular orientation of PM rotor d-axis.
+            harmonics: A numpy array holding holding harmonics of interest.
         Returns:
             b_radial: A numpy array of normal B fields in airgap at radius r and angle(s) alpha
         """
-
-        b_radial_h = self.radial_harmonics(r, theta, harmonics)
+        b_radial_h = self.radial_harmonics(r, harmonics)
         n = harmonics * self.p
         b_radial = self.__field_from_harmonics(b_radial_h, n, alpha)
         return b_radial
 
-    def tan(self, alpha: np.array, r, theta, harmonics):
+    def tan(self, alpha, r=None, harmonics=None):
         """Determines tangential B field at angle(s) alpha and radius r
 
         Args:
@@ -108,23 +115,27 @@ class SPM_InnerRotorPMField:
         Returns:
             b_tan: A numpy array of tangential B fields in airgap at radius r and angle(s) alpha
         """
-
-        b_tan_h = self.tan_harmonics(r, theta, harmonics)
+        b_tan_h = self.tan_harmonics(r, harmonics)
         n = harmonics * self.p
         b_tan = self.__field_from_harmonics(b_tan_h, n, alpha)
         return b_tan
 
-    def radial_harmonics(self, r, theta, harmonics):
+    def radial_harmonics(self, r=None, harmonics=None):
         """Determines radial B field harmonics at radius r
 
         Args:
-            r: Radius at which B field is calculated. Should be of type int or float
+            r: Radius at which B field is calculated. Should be of type int or float. Defaults to 
+              inner bore of stator if not defined.
             theta: angular orientation of PM rotor d-axis
-            harmonics: A numpy array holding holding harmonics of interest
+            harmonics: A numpy array holding holding harmonics of interest. Considers 1st thirteen
+              harmonics of p if not defined 
         Returns:
             b_rad_h: A numpy array of radial B field harmonics corresponding harmonics array
         """
-
+        if r==None:
+            r = self.Rsi    # stator inner bore
+        if harmonics==None:
+            harmonics = self.p * np.arange(1,15,2)  # first 13 harmonics
         r_fe = self.r_fe
         p = self.p
         muR = self.muR
@@ -142,7 +153,7 @@ class SPM_InnerRotorPMField:
         # discard even harmoincs and revise formula for vp=1
         for i in range(len(vp)):
             # even harmonics non-existent
-            if harmonics[i]%2==0:
+            if harmonics[i]/p % 2 == 0:
                 Bov[i] = 0
                 continue
             # vp=1 has different field formula
@@ -151,26 +162,31 @@ class SPM_InnerRotorPMField:
                             (r_fe/Rsi)**2*np.log((Rmo/r_fe)**2))*muR/((muR+1)*(1-(r_fe/Rsi)**2)-\
                             (muR-1)*((Rmo/Rsi)**2-(r_fe/Rmo)**2))*(1+(Rsi/r)**2))
         # rotate based on rotor orientation
-        b_rad_h = np.array(Bov) * np.exp(-theta*harmonics*p* 1j)
+        b_rad_h = np.array(Bov) * np.exp(-self.theta*harmonics*p* 1j)
         return b_rad_h 
     
-    def tan_harmonics(self, r, theta, harmonics):
+    def tan_harmonics(self, r=None, harmonics=None):
         """Determines tangential B field harmonics at radius r
 
         Args:
-            r: Radius at which B field is calculated. Should be of type int or float
+            r: Radius at which B field is calculated. Should be of type int or float. Defaults to 
+              inner bore of stator if not defined.
             theta: angular orientation of PM rotor d-axis
-            harmonics: A numpy array holding holding harmonics of interest
+            harmonics: A numpy array holding holding harmonics of interest. Considers 1st thirteen
+              harmonics of p if not defined 
         Returns:
             b_rad_h: A numpy array of tangential B field harmonics corresponding harmonics array
         """
-
+        if r==None:
+            r = self.Rsi    # stator inner bore
+        if harmonics==None:
+            harmonics = self.p * np.arange(1,15,2)  # first 13 harmonics
         r_fe = self.r_fe
         p = self.p
         muR = self.muR
         Rmo = self.Rmo # rotor outer radius
         Rsi = self.Rsi # stator inner radius
-        vp = harmonics*p
+        vp = harmonics
         # get magnetization vector
         Mv, c3v = self.__get_Mv_c3v(harmonics)
         # calculate tangential b field magnitudes
@@ -182,7 +198,7 @@ class SPM_InnerRotorPMField:
         # discard even harmoincs and revise formula for vp=1
         for i in range(len(vp)):
             # even harmonics non-existent
-            if harmonics[i]%2==0:
+            if harmonics[i]/p % 2 == 0:
                 Bov[i] = 0
                 continue
             # vp=1 has different field formula
@@ -191,17 +207,17 @@ class SPM_InnerRotorPMField:
                             (r_fe/Rsi)**2*np.log((Rmo/r_fe)**2))*muR/((muR+1)*(1-(r_fe/Rsi)**2)-\
                             (muR-1)*((Rmo/Rsi)**2-(r_fe/Rmo)**2))*(-1+(Rsi/r)**2)
         # rotate based on rotor orientation
-        b_tan_h = np.array(Bov) * np.exp(-theta*harmonics*p* 1j)
+        b_tan_h = np.array(Bov) * np.exp(-self.theta*harmonics*p* 1j)
         # rotate again considering tan is a sine function 
         b_tan_h = b_tan_h * np.exp(-np.pi/2*harmonics*p* 1j)
         return b_tan_h 
 
     def __get_Mv_c3v(self, harmonics):
-        v = harmonics
         alpha_p = self.alpha_p
         Br = self.Br
         p = self.p
-        vp = v*p
+        vp = harmonics
+        v = vp/p
         if self.mag_dir=="parallel":
             c1v = np.sin((vp+1)*alpha_p*np.pi/(2*p))/((vp+1)*alpha_p*np.pi/(2*p))
             c2v = np.sin((vp-1)*alpha_p*np.pi/(2*p))/((vp-1)*alpha_p*np.pi/(2*p))
