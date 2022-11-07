6b255f686ed29d05d5f81c9b77c7d0b5e8c367c9
Anvar Khamitov <khamitov@wisc.edu>
2022-11-07 16:14:48 -0600

    Add cross-section classes for stator's right and left slots

--- mach_cad/model_obj/cross_sects/inner_rotor_stator/__init__.py
+++ mach_cad/model_obj/cross_sects/inner_rotor_stator/__init__.py
@@ -5,7 +5,7 @@ from ...dimensions.dim_linear import DimLinear
 from ...dimensions.dim_angular import DimAngular
 from ..cross_sect_base import CrossSectBase, CrossSectToken
 
-__all__ = ['CrossSectInnerRotorStator']
+__all__ = ['CrossSectInnerRotorStator', 'CrossSectInnerRotorStatorRightSlot', 'CrossSectInnerRotorStatorLeftSlot']
 
 
 class CrossSectInnerRotorStator(CrossSectBase):
@@ -216,3 +216,227 @@ class CrossSectInnerRotorStator(CrossSectBase):
             pass
         else:
             raise TypeError("Q not of type int")
+
+
+
+class CrossSectInnerRotorStatorRightSlot(CrossSectBase):
+
+    def __init__(self, **kwargs: any) -> None:
+        '''
+        Intialization function for HalfSlot class. This function takes in
+        arguments and saves the information passed to private variable to make
+        them read-only
+        Parameters
+        ----------
+        **kwargs : any
+            DESCRIPTION. Keyword arguments provided to the initialization funcntion.
+            The following argument names have to be included in order for the code
+            to execute: name, dim_t, dim_r_o, location. 
+            
+        Returns
+        -------
+        None
+        '''
+        self._create_attr(kwargs)
+
+        super()._validate_attr()
+        self._validate_attr()
+
+    @property
+    def stator_core(self):
+        return self._stator_core
+
+    def draw(self, drawer):
+
+        alpha_st = DimRadian(self.stator_core.dim_alpha_st)
+        alpha_so = DimRadian(self.stator_core.dim_alpha_so)
+        r_si = self.stator_core.dim_r_si
+        d_so = self.stator_core.dim_d_so
+        d_sp = self.stator_core.dim_d_sp
+        d_st = self.stator_core.dim_d_st
+        d_sy = self.stator_core.dim_d_sy
+        w_st = self.stator_core.dim_w_st
+        r_st = self.stator_core.dim_r_st
+        r_sf = self.stator_core.dim_r_sf
+        r_sb = self.stator_core.dim_r_sb
+        Q = self.stator_core.Q
+
+        alpha_total = DimRadian(2 * np.pi / Q)
+
+        x1 = r_si * np.cos(alpha_st / 2)
+        beta2 = alpha_st / 2 - alpha_so
+        x2 = x1 + d_so * np.cos(beta2)
+        r3 = r_si + d_sp
+        beta3 = np.arcsin((w_st / 2) / r3)
+        x3 = r3 * np.cos(beta3)
+        r4 = r3 + d_st
+        beta4 = np.arcsin((w_st / 2) / r4)
+        x4 = r4 * np.cos(beta4)
+        x5 = r4 * np.cos(alpha_total / 2)
+
+        y1 = r_si * np.sin(alpha_st / 2)
+        y2 = y1 + d_so * np.sin(beta2)
+        y3 = w_st / 2
+        y4 = w_st / 2
+        y5 = r4 * np.sin(alpha_total / 2)
+
+        beta5 = alpha_total / 2 - DimRadian(np.arctan(y2 / x2))
+
+        x6 = x2 * np.cos(beta5) - y2 * np.sin(beta5)
+        y6 = x2 * np.sin(beta5) + y2 * np.cos(beta5)
+
+
+        x_arr = [x2, x3, x4, x5, x6]
+        y_arr = [y2, y3, y4, y5, y6]
+
+        seg1 = []
+        seg2 = []
+        arc1 = []
+        seg3 = []
+        arc2 = []
+
+        # transpose list
+        coords = [x_arr, y_arr]
+        coords = list(zip(*coords))
+        coords = [list(sublist) for sublist in coords]
+
+        # for i in range(0, 0):
+        p = self.location.transform_coords(coords, alpha_total * 0)
+        seg1.append(drawer.draw_line(p[0], p[1]))
+        seg2.append(drawer.draw_line(p[1], p[2]))
+        arc1.append(drawer.draw_arc(self.location.anchor_xy, p[2], p[3]))
+        seg3.append(drawer.draw_line(p[3], p[4]))            
+        arc2.append(drawer.draw_arc(self.location.anchor_xy, p[0], p[4]))
+
+        alpha_c = 2 * np.pi / Q * 1 / 2 - (np.arctan(y5 / x5) - np.arctan(y4 / x4)) / 2
+        xc = (x3 + x4) / 2 * np.cos(alpha_c)
+        yc = (x3 + x4) / 2 * np.sin(alpha_c)
+
+        inner_coord = self.location.transform_coords([[xc, yc]])
+        segments = [seg1, seg2, seg3, arc1, arc2]
+        segs = [x for segment in segments for x in segment]
+        cs_token = CrossSectToken(inner_coord[0], segs)
+        return cs_token
+
+    def _validate_attr(self):
+
+        if isinstance(self.stator_core, CrossSectInnerRotorStator):
+            pass
+        else:
+            raise TypeError("stator_core not of type CrossSectInnerRotorStator")
+
+
+
+class CrossSectInnerRotorStatorLeftSlot(CrossSectBase):
+
+    def __init__(self, **kwargs: any) -> None:
+        '''
+        Intialization function for HalfSlot class. This function takes in
+        arguments and saves the information passed to private variable to make
+        them read-only
+        Parameters
+        ----------
+        **kwargs : any
+            DESCRIPTION. Keyword arguments provided to the initialization funcntion.
+            The following argument names have to be included in order for the code
+            to execute: name, dim_t, dim_r_o, location. 
+            
+        Returns
+        -------
+        None
+        '''
+        self._create_attr(kwargs)
+
+        super()._validate_attr()
+        self._validate_attr()
+
+    @property
+    def stator_core(self):
+        return self._stator_core
+
+    def draw(self, drawer):
+
+        alpha_st = DimRadian(self.stator_core.dim_alpha_st)
+        alpha_so = DimRadian(self.stator_core.dim_alpha_so)
+        r_si = self.stator_core.dim_r_si
+        d_so = self.stator_core.dim_d_so
+        d_sp = self.stator_core.dim_d_sp
+        d_st = self.stator_core.dim_d_st
+        d_sy = self.stator_core.dim_d_sy
+        w_st = self.stator_core.dim_w_st
+        r_st = self.stator_core.dim_r_st
+        r_sf = self.stator_core.dim_r_sf
+        r_sb = self.stator_core.dim_r_sb
+        Q = self.stator_core.Q
+
+        alpha_total = DimRadian(2 * np.pi / Q)
+
+        x1 = r_si * np.cos(alpha_st / 2)
+        beta2 = alpha_st / 2 - alpha_so
+        x2 = x1 + d_so * np.cos(beta2)
+        r3 = r_si + d_sp
+        beta3 = np.arcsin((w_st / 2) / r3)
+        x3 = r3 * np.cos(beta3)
+        r4 = r3 + d_st
+        beta4 = np.arcsin((w_st / 2) / r4)
+        x4 = r4 * np.cos(beta4)
+        x5 = r4 * np.cos(alpha_total / 2)
+
+        y1 = r_si * np.sin(alpha_st / 2)
+        y2 = y1 + d_so * np.sin(beta2)
+        y3 = w_st / 2
+        y4 = w_st / 2
+        y5 = r4 * np.sin(alpha_total / 2)
+
+        beta5 = alpha_total / 2 - DimRadian(np.arctan(y2 / x2))
+
+        x6 = x2 * np.cos(beta5) - y2 * np.sin(beta5)
+        y6 = x2 * np.sin(beta5) + y2 * np.cos(beta5)
+
+
+        x_arr = [x2, x3, x4, x5, x6]
+        y_arr = [y2, y3, y4, y5, y6]
+            
+        seg1 = []
+        seg2 = []
+        arc1 = []
+        seg3 = []
+        arc2 = []
+
+        # transpose list
+        coords = [x_arr, y_arr]
+        coords = list(zip(*coords))
+        coords = [list(sublist) for sublist in coords]
+        coords = self.location.transform_coords(coords, alpha_total * - 1 / 2)
+        for i in range(0, len(coords)):
+            coords[i][1] = - coords[i][1]
+        coords = self.location.transform_coords(coords, alpha_total * 1 / 2)
+        for i in range(0, len(coords)):
+            x_arr[i] = coords[i][0]
+            y_arr[i] = coords[i][1]
+            
+        p = self.location.transform_coords(coords, alpha_total * 0)
+
+        seg1.append(drawer.draw_line(p[0], p[1]))
+        seg2.append(drawer.draw_line(p[1], p[2]))
+        arc1.append(drawer.draw_arc(self.location.anchor_xy, p[3], p[2]))
+        seg3.append(drawer.draw_line(p[3], p[4]))            
+        arc2.append(drawer.draw_arc(self.location.anchor_xy, p[4], p[0]))
+
+        alpha_c = 2 * np.pi / Q * 1 / 2 + (np.arctan(y5 / x5) - np.arctan(y4 / x4)) / 2
+        xc = (x3 + x4) / 2 * np.cos(alpha_c)
+        yc = (x3 + x4) / 2 * np.sin(alpha_c)
+
+        inner_coord = self.location.transform_coords([[xc, yc]])
+        segments = [seg1, seg2, arc1, seg3, arc2]
+        segs = [x for segment in segments for x in segment]
+        cs_token = CrossSectToken(inner_coord[0], segs)
+        return cs_token
+
+    def _validate_attr(self):
+
+        if isinstance(self.stator_core, CrossSectInnerRotorStator):
+            pass
+        else:
+            raise TypeError("stator_core not of type CrossSectInnerRotorStator")
+
