1a8435d41f46e34c7d172ec96f44e4264a27aea6
Anvar Khamitov <khamitov@wisc.edu>
2022-12-24 00:54:14 -0600

    Add scaling calculations in T2TSS post-analyzer

--- examples/mach_eval_examples/bim_eval/bim_transient_2tss_post_analyzer.py
+++ examples/mach_eval_examples/bim_eval/bim_transient_2tss_post_analyzer.py
@@ -15,6 +15,10 @@ from mach_eval.analyzers.torque_data import (
     ProcessTorqueDataProblem,
     ProcessTorqueDataAnalyzer,
 )
+from mach_eval.analyzers.mechanical.windage_loss import (
+    WindageLossProblem,
+    WindageLossAnalyzer,
+)
 
 
 class BIM_Transient_2TSS_PostAnalyzer:
@@ -24,13 +28,20 @@ class BIM_Transient_2TSS_PostAnalyzer:
         op_pt = state_out.design.settings
 
         ############################ Extract required info ###########################
-        bim_transient_2tss_analyzer = results["bim_transient_2tss_analyzer"]
-        no_of_steps_2nd_TSS = bim_transient_2tss_analyzer.config.no_of_steps_2nd_TSS
-        no_of_rev_2nd_TSS = bim_transient_2tss_analyzer.config.no_of_rev_2nd_TSS
+        config = results["analyzer_configurations"]
+        no_of_steps_2nd_TSS = config.no_of_steps_2nd_TSS
+        no_of_rev_2nd_TSS = config.no_of_rev_2nd_TSS
         number_of_total_steps = results["current"].shape[0]
         i1 = number_of_total_steps - no_of_steps_2nd_TSS # index where 2nd time step section begins
-        slip_freq = bim_transient_2tss_analyzer.slip_freq
-        drive_freq = bim_transient_2tss_analyzer.drive_freq
+        i2 = - int(no_of_steps_2nd_TSS / no_of_rev_2nd_TSS * 0.25) # index where last quarter period of 2nd time step section begins
+        slip_freq = results["slip_freq"]
+        drive_freq = results["drive_freq"]
+        omega_m = machine.omega_m
+        m = machine.no_of_phases
+        R_wdg = results["stator_wdg_resistances"][0]
+        R_wdg_coil_ends = results["stator_wdg_resistances"][1]
+        R_wdg_coil_sides = results["stator_wdg_resistances"][2]
+        l_st = machine.l_st
 
         results["current"] = results["current"].iloc[i1:]
         results["torque"] = results["torque"].iloc[i1:]
@@ -45,9 +56,16 @@ class BIM_Transient_2TSS_PostAnalyzer:
         rotor_mass = (
             machine.V_rfe * 1e-9 * machine.rotor_iron_mat["core_material_density"]
             + machine.V_shaft * 1e-9 * machine.shaft_mat["shaft_material_density"]
-            + machine.V_r_cage * 1e-9 * machine.rotor_bar_mat["bar_material_density"]
+            + results["V_r_cage"] * 1e-9 * machine.rotor_bar_mat["bar_material_density"]
         )
         rotor_volume = machine.V_rotor * 1e-9
+            # motor mass:
+            # weight = machine.V_rfe * rotor_iron['core_material_density'] + \
+            #          machine.V_sh * shaft['shaft_material_density'] + \
+            #          machine.V_rPM * magnet['magnet_material_density'] + \
+            #          machine.V_sfe * stator_iron['core_material_density'] + \
+            #          machine.V_scu * coil['copper_material_density']
+
         
         # Torque
         torque_prob = ProcessTorqueDataProblem(results["torque"]["TorCon"])
@@ -56,49 +74,87 @@ class BIM_Transient_2TSS_PostAnalyzer:
         TRV = torque_avg / rotor_volume
 
         # Force
-        force_problem = ProcessForceDataProblem(
-            Fx=results["force"][r"ForCon:X Component"],
-            Fy=results["force"][r"ForCon:Y Component"],
-        )
+        if r"ForCon:X Component" in results["force"]: # older jmag versions
+            Fx = results["force"][r"ForCon:X Component"]
+            Fy = results["force"][r"ForCon:Y Component"]
+        elif r"ForCon:1st" in results["force"]: # jmag version 21
+            Fx = results["force"][r"ForCon:1st"]
+            Fy = results["force"][r"ForCon:2nd"]
+
+        force_problem = ProcessForceDataProblem(Fx=Fx, Fy=Fy)
         force_analyzer = ProcessForceDataAnalyzer()
         Fx_avg, Fy_avg, F_abs_avg, Em, Ea = force_analyzer.analyze(force_problem)
-        FRW = F_abs_avg / rotor_mass
+        FRW = F_abs_avg / (9.81 * rotor_mass)
         FRV = F_abs_avg / rotor_volume
 
         # Losses
+        # From JMAG
         stator_iron_loss = results["iron_loss"]["StatorCore"][0]
         rotor_iron_loss = results["iron_loss"]["RotorCore"][0]
         stator_eddy_current_loss = results["eddy_current_loss"]["StatorCore"][0]
         rotor_eddy_current_loss = results["eddy_current_loss"]["RotorCore"][0]
         stator_hysteresis_loss= results["hysteresis_loss"]["StatorCore"][0]
         rotor_hysteresis_loss = results["hysteresis_loss"]["RotorCore"][0]
-        stator_ohmic_loss_along_stack = results["ohmic_loss"]["Coils"].iloc[i2:].mean()
+        stator_ohmic_loss = results["ohmic_loss"]["Coils"].iloc[i2:].mean()
+        stator_ohmic_loss_along_stack = stator_ohmic_loss * R_wdg_coil_sides / R_wdg
+        stator_ohmic_loss_end_wdg = stator_ohmic_loss * R_wdg_coil_ends / R_wdg
         rotor_ohmic_loss_along_stack = results["ohmic_loss"]["Cage"].iloc[i2:].mean()
         
-
-
-
-
-
-
-        stator_calc_ohmic_loss = results["stator_calc_ohmic_loss"][0]
-        stator_calc_ohmic_loss_along_stack = results["stator_calc_ohmic_loss"][1]
-        stator_calc_ohmic_loss_end_wdg = results["stator_calc_ohmic_loss"][2]
+        # Calculate stator winding ohmic losses
+        It_hat = op_pt.It_ratio * machine.rated_current * np.sqrt(2)
+        Is_hat = op_pt.Is_ratio * machine.rated_current * np.sqrt(2)
+        stator_calc_ohmic_loss = R_wdg * m / 2 * (It_hat ** 2 + Is_hat**2)
+        stator_calc_ohmic_loss_end_wdg = R_wdg_coil_ends * m / 2 * (It_hat ** 2 + Is_hat**2)
+        stator_calc_ohmic_loss_along_stack = R_wdg_coil_sides * m / 2 * (It_hat ** 2 + Is_hat**2)
 
         # Calculate rotor cage ohmic losses (using last 1/4th of a cycle)
-        i2 = - int(no_of_steps_2nd_TSS / no_of_rev_2nd_TSS * 0.25) # index where last quarter period of 2nd time step section begins
         currents = results["current"].iloc[i2:]
-        P1, P2, P3 = calculate_rotor_cage_ohmic_losses(machine, currents, bim_transient_2tss_analyzer.conductor_names, 
-                                                        bim_transient_2tss_analyzer.config.non_zero_end_ring_res)
+        R_bar = results["rotor_cage_resistances"][0]
+        R_end_ring = results["rotor_cage_resistances"][1]
+        P1, P2, P3 = calculate_rotor_cage_ohmic_losses(
+            machine, currents, results["conductor_names"], 
+            config.non_zero_end_ring_res, R_end_ring, R_bar)
         rotor_calc_ohmic_loss = P1
         rotor_calc_ohmic_loss_along_stack = P2
         rotor_calc_ohmic_loss_end_rings = P3
 
-        windage_loss = get_windage_loss(machine, op_pt)
+        # Scaling
+        if config.scale_axial_length == True:
+            expected_torque = machine.mech_power / omega_m
+            scale_ratio = expected_torque / torque_avg
+            l_st = l_st * scale_ratio
 
+            rotor_mass = scale_ratio * rotor_mass
+            rotor_volume = scale_ratio * rotor_volume
 
+            torque_avg = scale_ratio * torque_avg
+            results["breakdown_torque_from_tha"] = scale_ratio * results["breakdown_torque_from_tha"]
+            Fx_avg = scale_ratio * Fx_avg
+            Fy_avg = scale_ratio * Fy_avg
+            F_abs_avg = scale_ratio * F_abs_avg
 
+            stator_iron_loss = scale_ratio * stator_iron_loss
+            rotor_iron_loss = scale_ratio * rotor_iron_loss
+            stator_eddy_current_loss = scale_ratio * stator_eddy_current_loss
+            rotor_eddy_current_loss = scale_ratio * rotor_eddy_current_loss
+            stator_hysteresis_loss = scale_ratio * stator_hysteresis_loss
+            rotor_hysteresis_loss = scale_ratio * rotor_hysteresis_loss
+            stator_ohmic_loss_along_stack = scale_ratio * stator_ohmic_loss_along_stack
+            stator_ohmic_loss = stator_ohmic_loss_along_stack + stator_ohmic_loss_end_wdg
+            rotor_ohmic_loss_along_stack = scale_ratio * rotor_ohmic_loss_along_stack
+            stator_calc_ohmic_loss_along_stack = scale_ratio * stator_calc_ohmic_loss_along_stack
+            stator_calc_ohmic_loss = stator_calc_ohmic_loss_along_stack + stator_calc_ohmic_loss_end_wdg
+            rotor_calc_ohmic_loss_along_stack = scale_ratio * rotor_calc_ohmic_loss_along_stack
+            rotor_calc_ohmic_loss = rotor_calc_ohmic_loss_along_stack + rotor_calc_ohmic_loss_end_rings
 
+        # Calculate windage loss
+        windage_loss_prob = WindageLossProblem(
+            Omega=omega_m, R_ro=machine.r_ro/1000, stack_length=l_st/1000,
+            R_st=machine.r_si/1000, u_z=0, T_air=op_pt.ambient_temp
+            )
+        [windage_loss_radial, windage_loss_endFace, windage_loss_axial] = WindageLossAnalyzer.analyze(windage_loss_prob)
+        windage_loss = windage_loss_radial + windage_loss_endFace + windage_loss_axial
+        windage_loss_2 = get_windage_loss(machine, l_st, omega_m, TEMPERATURE_OF_AIR=op_pt.ambient_temp)
 
         # Total losses, output power, and efficiency
         total_losses = (
@@ -106,7 +162,7 @@ class BIM_Transient_2TSS_PostAnalyzer:
             stator_calc_ohmic_loss + rotor_calc_ohmic_loss + 
             windage_loss
         )
-        P_out = torque_avg * (drive_freq - slip_freq) / machine.p * 2 * np.pi
+        P_out = torque_avg * omega_m
         efficiency = P_out / (P_out + total_losses)
 
 
@@ -116,7 +172,10 @@ class BIM_Transient_2TSS_PostAnalyzer:
         post_processing["torque_ripple"] = torque_ripple
         post_processing["TRW"] = TRW
         post_processing["TRV"] = TRV
-        post_processing["slip_freq"] = slip_freq
+        post_processing["breakdown_slip_freq_tha"] = slip_freq
+        post_processing["breakdown_torque_from_tha"] = results["breakdown_torque_from_tha"]
+        post_processing["l_st"] = l_st
+        post_processing["scale_ratio"] = scale_ratio
 
         post_processing["Fx_avg"] = Fx_avg
         post_processing["Fy_avg"] = Fy_avg
@@ -125,6 +184,7 @@ class BIM_Transient_2TSS_PostAnalyzer:
         post_processing["FRV"] = FRV
         post_processing["rotor_mass"] = rotor_mass
         post_processing["rotor_volume"] = rotor_volume
+        # post_processing["motor_mass"] = motor_mass
         post_processing["Em"] = Em
         post_processing["Ea"] = Ea
 
@@ -134,7 +194,9 @@ class BIM_Transient_2TSS_PostAnalyzer:
         post_processing["rotor_eddy_current_loss"] = rotor_eddy_current_loss
         post_processing["stator_hysteresis_loss"] = stator_hysteresis_loss
         post_processing["rotor_hysteresis_loss"] = rotor_hysteresis_loss
-        post_processing["stator_ohmic_loss_along_stack"] =stator_ohmic_loss_along_stack
+        post_processing["stator_ohmic_loss"] = stator_ohmic_loss
+        post_processing["stator_ohmic_loss_along_stack"] = stator_ohmic_loss_along_stack
+        post_processing["stator_ohmic_loss_end_wdg"] = stator_ohmic_loss_end_wdg
         post_processing["rotor_ohmic_loss_along_stack"] = rotor_ohmic_loss_along_stack
         post_processing["stator_calc_ohmic_loss"] = stator_calc_ohmic_loss
         post_processing["stator_calc_ohmic_loss_along_stack"] = stator_calc_ohmic_loss_along_stack
@@ -150,31 +212,30 @@ class BIM_Transient_2TSS_PostAnalyzer:
         state_out.conditions.em = post_processing
 
         print("\n************************ EM RESULT ************************")
+        print("Scaling factor = ", scale_ratio)
         print("Torque = ", torque_avg, " Nm")
-        print("Torque density = ", torque_avg / (machine.V_rotor * 1e-9), " Nm/m3",)
+        print("Torque density = ", TRV, " Nm/m3",)
         print("Power = ", P_out, " W")
-        print("Efficiency = ", efficiency, " %")
+        print("Efficiency = ", efficiency * 100, " %")
 
         print("Force = ", F_abs_avg, " N")
-        print("Force per rotor weight = ", FRW / 9.8, " pu")
+        print("Force per rotor weight = ", FRW, " pu")
         print("Force error angle = ", Ea, " deg")
         print("************************************************************\n")
 
         return state_out
 
 
-def calculate_rotor_cage_ohmic_losses(machine, currents, conductor_names, non_zero_end_ring_res):
+def calculate_rotor_cage_ohmic_losses(machine, currents, conductor_names, non_zero_end_ring_res, R_end_ring, R_bar):
     ohmic_losses_bars = []
     ohmic_losses_end_ring1 = []
     ohmic_losses_end_ring2 = []
-    R_end_ring = machine.R_end_ring
-    R_rotor_bar = machine.R_rotor_bar
 
     phases = machine.name_phases_rotor
 
     for i in range(len(conductor_names)):
         ohmic_losses_bars.append(
-            (R_rotor_bar * currents[conductor_names[i]] ** 2).mean()
+            (R_bar * currents[conductor_names[i]] ** 2).mean()
         )
 
     if non_zero_end_ring_res == True:
@@ -201,12 +262,12 @@ def calculate_rotor_cage_ohmic_losses(machine, currents, conductor_names, non_ze
 
 
 # From Jiahao's code
-def get_windage_loss(machine, op_pt, TEMPERATURE_OF_AIR=75):
+def get_windage_loss(machine, l_st, Omega, TEMPERATURE_OF_AIR=25):
 
     # %Air friction loss calculation
     nu_0_Air  = 13.3e-6#;  %[m^2/s] kinematic viscosity of air at 0
     rho_0_Air = 1.29#;     %[kg/m^3] Air density at 0
-    Shaft = [machine.l_st,                               #1;         %End position of the sections mm (Absolut)
+    Shaft = [l_st,                               #1;         %End position of the sections mm (Absolut)
              machine.r_ro + machine.delta_e, #1;         %Inner Radius in mm
              1,                                                     #0;         %Shrouded (1) or free surface (0)
              machine.delta_e]                              #0];        %Airgap in mm
@@ -221,8 +282,6 @@ def get_windage_loss(machine, op_pt, TEMPERATURE_OF_AIR=75):
     L     = Shaft[0]*1e-3 # in meter
     R     = Shaft[1]*1e-3 # radius of air gap
     delta = Shaft[3]*1e-3 # number_of_total_steps of air gap
-    
-    Omega = (op_pt.drive_freq - op_pt.slip_freq) / machine.p * 2 * np.pi
 
     # Reynolds number
     Rey = R**2 * (Omega)/nu_Air
