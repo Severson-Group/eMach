384640b227d427a02239f83d1621fe05fd051648
Anson Chan <wchan29@wisc.edu>
2023-03-13 15:13:54 -0500

    Edit rotor_speed_limit.py

--- mach_eval/analyzers/mechanical/rotor_speed_limit.py
+++ mach_eval/analyzers/mechanical/rotor_speed_limit.py
@@ -1,1 +1,21 @@
 import numpy as np
+import rotor_structural as rs
+
+class SPM_RotorSpeedLimitProblem:
+    """Problem class for SPM_RotorSpeedLimitAnalyzer
+    
+    Attributes:
+
+    """
+
+    def __init__(
+        self,
+        r_sh: float,
+        d_m: float,
+        r_ro: float,
+        d_sl: float,
+        delta_sl: float,
+        deltaT: float,
+        N: float,
+        ) -> "SPM_RotorSpeedLimitProblem":
+        pass
