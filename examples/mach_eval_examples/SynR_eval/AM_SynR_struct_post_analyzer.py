820f5da09a5ffeae3dad54ddd575424ed86d6e68
Dante Newman <dmnewman3@wisc.edu>
2023-05-08 15:42:35 -0500

    Adjust for new rotor materials

--- examples/mach_eval_examples/SynR_eval/AM_SynR_struct_post_analyzer.py
+++ examples/mach_eval_examples/SynR_eval/AM_SynR_struct_post_analyzer.py
@@ -3,12 +3,13 @@ import copy
 class AM_SynR_Struct_PostAnalyzer:
     def get_next_state(results, in_state):
         state_out = copy.deepcopy(in_state)
+        machine = state_out.design.machine
 
         ############################ Extract required info ###########################
         struct = results["max_stress"]
         s = struct['Maximum Value']
         max_stress = s[0]
-        yield_stress = 300 * 1000000
+        yield_stress = machine.rotor_barrier_mat["rotor_barrier_yield_stress"]
 
         ############################ Output #################################
         print("\n************************ STRUCTURAL RESULT ************************")
