c919a727ad7ae51a2280d3f73008331b517ac67b
Dante Newman <dmnewman3@wisc.edu>
2023-05-12 18:17:38 -0500

    edit SynR designer

--- examples/mach_opt_examples/SynR_opt/SynR_designer.py
+++ examples/mach_opt_examples/SynR_opt/SynR_designer.py
@@ -1,10 +1,7 @@
 import os
 import sys
 
-# change current working directory to file location
-sys.path.append(os.path.dirname(__file__)+"/../../..")
-
-from SynR_architect import SynR_Architect1
+from SynR_architect import AM_SynR_Architect
 from mach_eval.machines.materials.electric_steels import Arnon5
 from mach_eval.machines.materials.AM_materials import (
     Fe3Si,
@@ -14,19 +11,18 @@ from mach_eval.machines.materials.miscellaneous_materials import (
     Copper,
     Air,
 )
-from SynR_settings_handler import SynR_Settings_Handler
+from SynR_settings_handler import AM_SynR_Settings_Handler
 from mach_eval import MachineDesigner
 
-# specify machine specs
-design_spec = {
-    "p": 2,
-    "Q": 12,
-    "name": "Example_SynR_Machine",
-    "rated_speed": 1800,
-    "rated_current": 20,   
+# Specify machine specifications
+SynR_parameters = {
+    'p': 2,
+    'Q': 12,
+    'rated_speed': 1800,
+    'rated_current': 20,   
 }
 
-materials = {
+SynR_materials = {
     "air_mat": Air,
     "rotor_iron_mat": Fe3Si,
     "rotor_barrier_mat": L316,
@@ -35,8 +31,22 @@ materials = {
     "shaft_mat": L316,
 }
 
+SynR_winding = {
+    "no_of_layers": 2,
+    "layer_phases": [ ['U', 'W', 'V', 'U', 'W', 'V', 'U', 'W', 'V', 'U', 'W', 'V'],
+                        ['W', 'V', 'U', 'W', 'V', 'U', 'W', 'V', 'U', 'W', 'V', 'U'] ],
+    "layer_polarity": [ ['+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-'],
+                        ['-', '+', '-', '+', '-', '+', '-', '+', '-', '+', '-', '+'] ],
+    "pitch": 2,
+    "Z_q": 20,
+    "Kov": 1.8,
+    "Kcu": 0.5,
+    "phase_current_offset": 0,
+}
+
+
 # initialize BSPMArchitect with machine specification
-arch = SynR_Architect1(design_spec, materials)
-set_handler = SynR_Settings_Handler()
+arch = AM_SynR_Architect(SynR_parameters, SynR_materials, SynR_winding)
+set_handler = AM_SynR_Settings_Handler()
 
 designer = MachineDesigner(arch, set_handler)
