07c1aa20dee50fdfec1650b8e151fee2d26da9a0
Dante Newman <dmnewman3@wisc.edu>
2023-06-08 14:19:02 -0500

    Update rotor materials

--- mach_eval/machines/materials/AM_materials.py
+++ mach_eval/machines/materials/AM_materials.py
@@ -6,17 +6,14 @@ Fe3Si = {
     'rotor_iron_youngs_modulus'        : 185E9, # Pa
     'rotor_iron_poission_ratio'        : .3, 
     'rotor_iron_material_cost'         : 17087, # $/m3
-    'rotor_iron_ironloss_file_50'      : os.path.dirname(__file__) + '/Fe3Si_loss_50.iron',
-    'rotor_iron_ironloss_file_60'      : os.path.dirname(__file__) + '/Fe3Si_loss_60.iron',
-    'rotor_iron_ironloss_file_100'     : os.path.dirname(__file__) + '/Fe3Si_loss_100.iron',
-    'rotor_iron_ironloss_file_400'     : os.path.dirname(__file__) + '/Fe3Si_loss_400.iron',
-    'rotor_iron_hys_file_500'          : os.path.dirname(__file__) + '/Fe3Si_500.hys',
-    'rotor_iron_hys_file_1000'         : os.path.dirname(__file__) + '/Fe3Si_1000.hys',
-    'rotor_iron_hys_file_1500'         : os.path.dirname(__file__) + '/Fe3Si_1500.hys',
-    'rotor_iron_therm_conductivity'    : 28, # W/m-k
-    'rotor_iron_stacking_factor'       : 100, # percentage
+    'rotor_iron_ironloss_a'            : 1.12,
+    'rotor_iron_ironloss_b'            : 2.10,
+    'rotor_iron_ironloss_Kh'           : 91.8, # W/m3
+    'rotor_iron_ironloss_Ke'           : 0.41, # W/m3
+    'rotor_iron_therm_conductivity'    : 25, # W/m-k
+    'rotor_iron_stacking_factor'       : 96, # percentage
     'rotor_iron_bh_file'               : os.path.dirname(__file__) + '/Fe3Si.BH',
-    'alpha_Fe3Si'                   : 1.2e-5,
+    'alpha_Fe3Si'                      : 1.2e-5,
     }
 
 L316 = {
@@ -28,5 +25,5 @@ L316 = {
     'rotor_barrier_shear_modulus'        : 206E9 / (2 * (1 + .3)),
     'rotor_barrier_yield_stress'         : 500E9,
     'rotor_barrier_therm_conductivity'   : 51.9, # W/m-k  
-    'alpha_L316'                   : 1.2e-5
+    'alpha_L316'                         : 1.2e-5
     }
