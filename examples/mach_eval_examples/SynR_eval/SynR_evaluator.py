ca337427e6f1f89115f9cd938f88a088fd3a64d1
ENGR\dmnewman3 <dmnewman3@wisc.edu>
2023-06-09 16:39:14 -0500

    Update SynR Opt analyzer and optimization

--- examples/mach_eval_examples/SynR_eval/SynR_evaluator.py
+++ examples/mach_eval_examples/SynR_eval/SynR_evaluator.py
@@ -6,13 +6,13 @@ os.chdir(os.path.dirname(__file__))
 sys.path.append("../../../")
 
 from mach_eval import (MachineEvaluator, MachineDesign)
-from electromagnetic_step import electromagnetic_step
+from optimization_step import optimization_step
 from example_SynR_machine import Example_SynR_Machine, Machine_Op_Pt
 
-############################ Create Evaluator #####################
+############################ Create Evaluator ########################
 SynR_evaluator = MachineEvaluator(
     [
-        electromagnetic_step
+        optimization_step
     ]
 )
 
@@ -22,4 +22,4 @@ tic = clock_time()
 results = SynR_evaluator.evaluate(design_variant)
 toc = clock_time()
 
-print("Time spent on SynR evaluation is %g min." % ((toc- tic)/60))
+print("Time spent on SynR evaluation is %g min." % ((toc - tic)/60))
