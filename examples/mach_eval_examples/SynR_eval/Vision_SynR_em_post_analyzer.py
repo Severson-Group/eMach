fbb465ed66f08fbb1cba92358ff0bf01fdcad144
Dante Newman <dmnewman3@wisc.edu>
2023-06-28 11:16:56 -0500

    Add initial commit

---
+++ examples/mach_eval_examples/SynR_eval/Vision_SynR_em_post_analyzer.py
@@ -0,0 +1,127 @@
+import copy
+import numpy as np
+import os
+import sys
+
+from mach_eval.analyzers.torque_data import (
+    ProcessTorqueDataProblem,
+    ProcessTorqueDataAnalyzer,
+)
+from mach_eval.analyzers.mechanical.windage_loss import (
+    WindageLossProblem,
+    WindageLossAnalyzer,
+)
+
+class Vision_SynR_EM_PostAnalyzer:
+    def copper_loss(self):
+        return 3 * (self.I ** 2) * (self.R_wdg + self.R_wdg_coil_ends + self.R_wdg_coil_sides)
+
+    def get_next_state(results, in_state):
+        state_out = copy.deepcopy(in_state)
+        machine = state_out.design.machine
+        op_pt = state_out.design.settings
+
+        ############################ Extract required info ###########################
+        no_of_steps = results["no_of_steps"]
+        no_of_rev = results["no_of_rev"]
+        number_of_total_steps = results["current"].shape[0]
+        i1 = number_of_total_steps - no_of_steps
+        i2 = - int(no_of_steps / no_of_rev * 0.25)
+        omega_m = machine.omega_m
+        m = 3
+        drive_freq = results["drive_freq"]
+        R_wdg = results["stator_wdg_resistances"][0]
+        R_wdg_coil_ends = results["stator_wdg_resistances"][1]
+        R_wdg_coil_sides = results["stator_wdg_resistances"][2]
+
+        results["current"] = results["current"].iloc[i1:]
+        results["torque"] = results["torque"].iloc[i1:]
+        results["iron_loss"] = results["iron_loss"]
+        results["hysteresis_loss"] = results["hysteresis_loss"]
+        results["eddy_current_loss"] = results["eddy_current_loss"]
+
+        ############################ calculating volumes ###########################
+        machine = state_out.design.machine
+        V_sh = np.pi*(machine.r_sh**2)*machine.l_st
+        V_rfe = machine.l_st * (np.pi * (machine.r_ro ** 2 - machine.r_ri**2) - 2 * machine.p * (machine.w_b1 * (2 * machine.l_b1 + machine.l_b4) + machine.w_b2 * (2 * machine.l_b2 + machine.l_b5) + machine.w_b3 * (2 * machine.l_b3 + machine.l_b6)))
+
+        ############################ Post-processing #################################
+        rotor_mass = (
+            V_rfe * 1e-9 * machine.rotor_iron_mat["rotor_iron_material_density"]
+            + V_sh * 1e-9 * machine.shaft_mat["shaft_material_density"]
+        )
+        rotor_volume = (V_rfe + V_sh) * 1e-9
+
+        ############################ post processing ###########################
+        # Torque
+        torque_prob = ProcessTorqueDataProblem(results["torque"]["TorCon"])
+        torque_analyzer = ProcessTorqueDataAnalyzer()
+        torque_avg, torque_ripple = torque_analyzer.analyze(torque_prob)
+        TRW = torque_avg / rotor_mass
+        TRV = torque_avg / rotor_volume
+        PRW = TRW * omega_m
+        PRV = TRV * omega_m
+
+        # Windage
+        windage_loss_prob = WindageLossProblem(
+            Omega=omega_m, R_ro=machine.r_ro/1000, stack_length=machine.l_st/1000,
+            R_st=machine.r_si/1000, u_z=0, T_air=op_pt.ambient_temp
+            )
+        [windage_loss_radial, windage_loss_endFace, windage_loss_axial] = WindageLossAnalyzer.analyze(windage_loss_prob)
+
+        # Losses
+        # From JMAG
+        stator_iron_loss = results["iron_loss"]["StatorCore"][0]
+        rotor_iron_loss = results["iron_loss"]["RotorCore"][0]
+        stator_eddy_current_loss = results["eddy_current_loss"]["StatorCore"][0]
+        rotor_eddy_current_loss = results["eddy_current_loss"]["RotorCore"][0]
+        stator_hysteresis_loss= results["hysteresis_loss"]["StatorCore"][0]
+        rotor_hysteresis_loss = results["hysteresis_loss"]["RotorCore"][0]
+        stator_ohmic_loss = results["ohmic_loss"]["Coils"].iloc[i2:].mean()
+        windage_loss = windage_loss_axial + windage_loss_endFace + windage_loss_radial
+        
+        # Calculate stator winding ohmic losses
+        I_hat = machine.rated_current * op_pt.current_ratio
+        stator_calc_ohmic_loss = R_wdg * m / 2 * I_hat ** 2
+
+        # Total losses, output power, and efficiency
+        total_losses = (
+            stator_hysteresis_loss + rotor_hysteresis_loss + stator_calc_ohmic_loss + windage_loss)
+        P_out = torque_avg * omega_m
+        efficiency = P_out / (P_out + total_losses)
+
+        ############################ Output #################################
+        post_processing = {}
+        post_processing["torque_avg"] = torque_avg
+        post_processing["torque_ripple"] = torque_ripple
+        post_processing["TRW"] = TRW
+        post_processing["TRV"] = TRV
+        post_processing["PRW"] = PRW
+        post_processing["PRV"] = PRV
+        post_processing["l_st"] = machine.l_st
+        post_processing["rotor_mass"] = rotor_mass
+        post_processing["rotor_volume"] = rotor_volume
+        post_processing["stator_iron_loss"] = stator_iron_loss
+        post_processing["rotor_iron_loss"] = rotor_iron_loss
+        post_processing["stator_eddy_current_loss"] = stator_eddy_current_loss
+        post_processing["rotor_eddy_current_loss"] = rotor_eddy_current_loss
+        post_processing["stator_hysteresis_loss"] = stator_hysteresis_loss
+        post_processing["rotor_hysteresis_loss"] = rotor_hysteresis_loss
+        post_processing["stator_ohmic_loss"] = stator_ohmic_loss
+        post_processing["stator_calc_ohmic_loss"] = stator_calc_ohmic_loss
+        post_processing["total_losses"] = total_losses
+        post_processing["output_power"] = P_out
+        post_processing["efficiency"] = efficiency
+
+        state_out.conditions.em = post_processing
+
+        print("\n************************ ELECTROMAGNETIC RESULTS ************************")
+        #print("Torque = ", torque_avg, " Nm")
+        print("Torque density = ", TRV, " Nm/m3",)
+        print("Torque ripple = ", torque_ripple)
+        #print("Power = ", P_out, " W")
+        print("Power density = ", PRV, " W/m3",)
+        print("Efficiency = ", efficiency * 100, " %")
+        print("*************************************************************************\n")
+
+        return state_out
---
+++ mach_eval/machines/materials/AM_materials.py
@@ -0,0 +1,29 @@
+import os
+
+Fe3Si = {
+    'rotor_iron_material'              : 'Fe3Si', 
+    'rotor_iron_material_density'      : 7650, # kg/m3
+    'rotor_iron_youngs_modulus'        : 185E9, # Pa
+    'rotor_iron_poission_ratio'        : .3, 
+    'rotor_iron_material_cost'         : 17087, # $/m3
+    'rotor_iron_ironloss_a'            : 1.58,
+    'rotor_iron_ironloss_b'            : 1.17,
+    'rotor_iron_ironloss_Kh'           : 78.94, # W/m3
+    'rotor_iron_ironloss_Ke'           : 0.0372, # W/m3
+    'rotor_iron_therm_conductivity'    : 28, # W/m-k
+    'rotor_iron_stacking_factor'       : 96, # percentage
+    'rotor_iron_bh_file'               : os.path.dirname(__file__) + '/Fe3Si.BH',
+    'alpha_Fe3Si'                      : 1.2e-5,
+    }
+
+L316 = {
+    'rotor_barrier_material'             : 'L316',
+    'rotor_barrier_material_density'     : 7870, # kg/m3
+    'rotor_barrier_permeability'         : 1,
+    'rotor_barrier_youngs_modulus'       : 206E9, #Pa
+    'rotor_barrier_poission_ratio'       : .3, #[]
+    'rotor_barrier_shear_modulus'        : 206E9 / (2 * (1 + .3)),
+    'rotor_barrier_yield_stress'         : 500E9,
+    'rotor_barrier_therm_conductivity'   : 51.9, # W/m-k  
+    'alpha_L316'                         : 1.2e-5
+    }
