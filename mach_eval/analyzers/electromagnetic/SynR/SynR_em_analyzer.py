5e8a6ebed00cb15addf8748db5659444046c3080
Dante Newman <dmnewman3@wisc.edu>
2023-09-18 16:25:23 -0500

    update documentation for SynR analyzer

--- mach_eval/analyzers/electromagnetic/SynR/SynR_em_analyzer.py
+++ mach_eval/analyzers/electromagnetic/SynR/SynR_em_analyzer.py
@@ -36,7 +36,7 @@ class SynR_EM_Problem:
         x2 = (self.machine.r_ri + self.machine.d_r1 + self.machine.w_b1 + self.machine.d_r2 + self.machine.w_b2/2 - self.machine.l_b5/2)*np.cos(np.pi/4)
         y2 = self.machine.l_b2 + self.machine.w_b2/2 + (self.machine.r_ri + self.machine.d_r1 + self.machine.w_b1 + self.machine.d_r2 + self.machine.l_b5/2)*np.cos(np.pi/4)
         r_ro_compare2 = np.sqrt(x2**2 + y2**2)
-        if r_ro_compare1 < 0.975*self.machine.r_ro and r_ro_compare2 < 0.975*self.machine.r_ro and self.machine.l_b4 > 1.25*self.machine.w_b1 and self.machine.l_b5 > 1.25*self.machine.w_b2:
+        if r_ro_compare1 < 0.99*self.machine.r_ro and r_ro_compare2 < 0.99*self.machine.r_ro and self.machine.l_b4 > 1.25*self.machine.w_b1 and self.machine.l_b5 > 1.25*self.machine.w_b2:
             print("\nGeometry is valid!")
             print("\n")
         else:
@@ -232,7 +232,7 @@ class SynR_EM_Analyzer:
         ####################################################
 
         rotor_rotation = mo.DimDegree(-180 / (2 * self.machine_variant.p))
-        stator_rotation = mo.DimDegree(-(180+720) / self.machine_variant.Q)
+        stator_rotation = mo.DimDegree(-180 / self.machine_variant.Q)
 
         self.stator_core = mo.CrossSectInnerRotorStatorPartial(
             name="StatorCore",
