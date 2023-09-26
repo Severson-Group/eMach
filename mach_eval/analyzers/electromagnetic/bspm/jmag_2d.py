3ef05582d40fc5908df0ebfad8bd19148954950b
Anvar Khamitov <khamitov@wisc.edu>
2023-09-26 14:23:24 -0500

    Remove comma to avoid making `copper_loss` a tuple

--- mach_eval/analyzers/electromagnetic/bspm/jmag_2d.py
+++ mach_eval/analyzers/electromagnetic/bspm/jmag_2d.py
@@ -166,9 +166,10 @@ class BSPM_EM_Analyzer:
     @property
     def copper_loss(self):
         copper_loss_per_phase = (
-            ((self.current_trms / 2) ** 2 + self.current_srms**2) * self.R_coil * self.z_C
+            ((self.current_trms / 2) ** 2 + self.current_srms ** 2) * self.R_coil * self.z_C
         )
-        copper_loss = self.m * copper_loss_per_phase,
+
+        copper_loss = self.m * copper_loss_per_phase
         return copper_loss
 
     def draw_machine(self, toolJd):
